ec18022d0e29b26e8a91d50abed3f7153041c297
Rad-Sam <129858230+Rad-Sam@users.noreply.github.com>
2024-11-18 16:29:48 +0100

    Add files via upload

---
+++ clockinallan.ts
@@ -0,0 +1,91 @@
+const puppeteer = require("puppeteer"); // v22.0.0 or later
+
+(async () => {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  const timeout = 150000;
+  page.setDefaultTimeout(timeout);
+
+  {
+    const targetPage = page;
+    await targetPage.setViewport({
+      width: 993,
+      height: 750,
+    });
+  }
+  {
+    const targetPage = page;
+    const promises = [];
+    const startWaitingForEvents = () => {
+      promises.push(targetPage.waitForNavigation());
+    };
+    startWaitingForEvents();
+    await targetPage.goto("https://appx.wheniwork.com/");
+    await Promise.all(promises);
+  }
+  {
+    await page.waitForSelector("#email");
+    await page.waitForSelector("#password");
+    await Promise.all([
+      await page.type("#email", "ofworkin@gmail.com"),
+      await page.type("#password", "123onlyfans"),
+      page.click(".btn-login"), // Clicking the link will indirectly cause a navigation
+    ]);
+  }
+  {
+    const targetPage = page;
+    await puppeteer.Locator.race([
+      targetPage.locator("li.dropdown-starts-left > button > i"),
+      targetPage.locator(
+        '::-p-xpath(//*[@id=\\"navbarSupportedContent\\"]/ul/li[3]/button/i)'
+      ),
+      targetPage.locator(":scope >>> li.dropdown-starts-left > button > i"),
+    ])
+      .setTimeout(timeout)
+      .click();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+  {
+    const targetPage = page;
+    await puppeteer.Locator.race([
+      targetPage.locator("::-p-text(Clock In)"),
+      targetPage.locator(
+        "li.dropdown-starts-left > ul > li:nth-of-type(2) span"
+      ),
+      targetPage.locator(
+        '::-p-xpath(//*[@id=\\"navbarSupportedContent\\"]/ul/li[2]/ul/li[2]/a/span)'
+      ),
+      targetPage.locator(
+        ":scope >>> li.dropdown-starts-left > ul > li:nth-of-type(2) span"
+      ),
+    ])
+      .setTimeout(timeout)
+      .click({
+        offset: {
+          x: 23.925003051757812,
+          y: 8.599990844726562,
+        },
+      });
+  }
+  try {
+    await page.waitForSelector("button.dialog-option-wide", { timeout: 7000 });
+    const button = await page.$("button.dialog-option-wide");
+    await button.click();
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  } catch (error) {
+    // If the element is not found, this block will execute
+    // No action is needed here if you want to silently ignore the error
+  }
+
+  {
+    await page.waitForSelector("button.btn.btn-primary.btn-md[type='submit']");
+    const button = await page.$('button.btn.btn-primary.btn-md[type="submit"]');
+    await button.click();
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+  }
+
+  await browser.close();
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
---
+++ clockoutallan.ts
@@ -0,0 +1,84 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+  const timeout = 150000;
+  page.setDefaultTimeout(timeout);
+
+  {
+    const targetPage = page;
+    await targetPage.setViewport({
+      width: 993,
+      height: 750,
+    });
+  }
+  {
+    const targetPage = page;
+    const promises = [];
+    const startWaitingForEvents = () => {
+      promises.push(targetPage.waitForNavigation());
+    };
+    startWaitingForEvents();
+    await targetPage.goto("https://appx.wheniwork.com/");
+    await Promise.all(promises);
+  }
+  {
+    await page.waitForSelector("#email");
+    await page.waitForSelector("#password");
+    await Promise.all([
+      await page.type("#email", "ofworkin@gmail.com"),
+      await page.type("#password", "123onlyfans"),
+      page.click(".btn-login"), // Clicking the link will indirectly cause a navigation
+    ]);
+  }
+  {
+    const targetPage = page;
+    await puppeteer.Locator.race([
+      targetPage.locator("li.dropdown-starts-left > button > i"),
+      targetPage.locator(
+        '::-p-xpath(//*[@id=\\"navbarSupportedContent\\"]/ul/li[3]/button/i)'
+      ),
+      targetPage.locator(":scope >>> li.dropdown-starts-left > button > i"),
+    ])
+      .setTimeout(timeout)
+      .click();
+  }
+  {
+    const targetPage = page;
+    await puppeteer.Locator.race([
+      targetPage.locator("::-p-text(Clock Out)"),
+      targetPage.locator("li.dropdown-starts-left li:nth-of-type(3) span"),
+    ])
+      .setTimeout(timeout)
+      .click();
+  }
+  try {
+    // Try to click "yes" if it appears within the timeout period
+    await page.waitForSelector('label[for="given-break-opportunity_yes"]', {
+      timeout: 7000,
+    });
+    await page.click('label[for="given-break-opportunity_yes"]');
+  } catch (error) {
+    // If "yes" doesn't appear within the timeout, this block will execute
+  }
+  {
+    const targetPage = page;
+    await puppeteer.Locator.race([
+      targetPage.locator(
+        '::-p-aria(CLOCK OUT) >>>> ::-p-aria([role=\\"generic\\"])'
+      ),
+      targetPage.locator('button[type="submit"].btn.btn-primary.btn-md:has-text("Clock Out")'), targetPage.locator(':has-text("Clock Out")'),
+    ])
+      .setTimeout(timeout)
+      .click();
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
+
+  await browser.close();
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
